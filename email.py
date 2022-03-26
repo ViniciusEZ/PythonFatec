003303f750007142e937586b065469249f7f8435
ViniciusEZ <102422645+ViniciusEZ@users.noreply.github.com>
2022-03-26 16:40:40 -0300

    commit inicial
    este software tem por finalidade cadastrar usuários em um data base

---
+++ email.py
@@ -0,0 +1,10 @@
+email_fixo = "ez998@outlook.com"
+email_do_usuario = input("Digite o seu email: ")
+
+
+if email_do_usuario == email_fixo:
+    print("Acesso permitido!")
+else:
+    print("Acesso negado!")
+    
+    
