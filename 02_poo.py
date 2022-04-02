4e7a718cada2bf4a31c76f2a5e1d9f5e719d4837
ViniciusEZ <102422645+ViniciusEZ@users.noreply.github.com>
2022-04-02 10:42:04 -0300

    Update 02_poo.py

---
+++ 02_poo.py
@@ -0,0 +1,11 @@
+class Dog:
+    # ações     
+    # latir
+    comer = "comendo..."
+    dormir = "dormindo..."
+    morder = "mordendo..."
+    
+    # estado
+    isacordado = 
+    # issarna 
+    
